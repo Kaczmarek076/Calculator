0e8a7b8081f11875c00babb649cb4f0d3ee51fed
Kaczmarek076 <sebkac01@easv365.dk>
2025-11-10 10:41:36 +0100

    New claculator class

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -3,4 +3,9 @@ public class Calculator {
     public static void welcome() {
         System.out.println("Welcome to Calculator");
     }
+
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
 }
