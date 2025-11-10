2244d3b1f899260abd9b62c6f4fcfe501cb47b16
soer6589 <soemar01@easv365.dk>
2025-11-10 11:27:55 +0100

    bgfdb

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,10 +1,9 @@
 public class Calculator {
 
-    public static boolean pyth (double a, double b, double c){
-        if (a * a + b * b == c * c) {
-            return true;
-        }
+    public static void welcome() {
+        System.out.println("Welcome to Calculator");
+        System.out.println("Welcome to Calculator2");
+        scout
 
-        return false;
     }
 }
